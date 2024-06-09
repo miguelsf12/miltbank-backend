c524284dd4b4ee6700e8a69f2717c7e51841b5b6
Miguel Santos <miguelsantosf12@gmail.com>
2024-06-09 18:53:08 -0300

    feat: adicionado campo de telefone

--- models/User.js
+++ models/User.js
@@ -51,6 +51,10 @@ const UserSchema = new Schema({
     type: String,
     required: true
   },
+  telefone: {
+    type: String,
+    required: true
+  },
   password: {
     type: String,
     required: true
