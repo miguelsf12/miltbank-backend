5160a4aa17c4f26b6232fcbf8e321bdcdd4306d0
Miguel Santos <miguelsantosf12@gmail.com>
2024-06-18 15:16:32 -0300

    feat: helper de upload de images com multer

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,12 +3,13 @@ const UserController = require('../controllers/UserController')
 
 // Middlewares
 const verifyToken = require('../helpers/verify-token')
+const { imageUpload } = require('../helpers/image-upload')
 
 router.post('/forgotpassword', UserController.forgotpassword)
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', verifyToken, UserController.checkUser)
 router.get('/:id', UserController.getUserById)
-router.patch('/edit', verifyToken, UserController.editUser)
+router.patch('/edit', verifyToken, imageUpload.single('image'), UserController.editUser)
 
 module.exports = router
